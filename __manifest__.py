64b07812bf18a7388d328c4eb4279257264161b1
abdelhakim-85 <knis.abdelhakim85@gmail.com>
2018-11-01 11:04:54 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'noor_it_website',
+    'summary': 'noor_it_website',
+    'description': 'dev info',
+    'category': 'Theme',
+    'version': '1.0',
+    'author': 'Tunisofts - Abdelhakim Knis',
+    'licence':'GRL',
+    'Website':'',
+    'depends': ['website','web' ],
+    'data': [
+        'views/index.xml',
+        'views/about.xml',              
+   
+    ],
+   
+}
